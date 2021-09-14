c454083c280df4020c50288a40a6569735f32175
M&M <goldtextwitch@outlook.com>
2021-09-13 21:33:24 -0700

    Rename variables based off leftover xmls in SCU

--- ScrambleCriKey/ScrambleCriKey.cpp
+++ ScrambleCriKey/ScrambleCriKey.cpp
@@ -3,19 +3,19 @@ using namespace std;
 
 int main()
 {
-	uint64_t key = 0;
-	uint16_t scramblekey = 0;
-	uint64_t truekey = 0;
+	uint64_t KeyCode = 0;
+	uint16_t AwbHash = 0;
+	uint64_t AcbKey = 0;
 
-	cout << "key: ";
-	cin >> key;
+	cout << "Key Code: ";
+	cin >> KeyCode;
 
-	cout << "scramble key: ";
-	cin >> scramblekey;
+	cout << "AWB Hash: ";
+	cin >> AwbHash;
 
 	// https://github.com/hozuki/libcgss/issues/4#issuecomment-429415659
 	// https://blog.mottomo.moe/categories/Tech/RE/en/2018-10-12-New-HCA-Encryption/
-	truekey = key * ((uint64_t)(scramblekey << 16) | (uint16_t)(~scramblekey + 2));
+	AcbKey = KeyCode * ((uint64_t)(AwbHash << 16) | (uint16_t)(~AwbHash + 2));
 
-	cout << "true key: " << truekey;
+	cout << "ACB Key: " << AcbKey;
 }
